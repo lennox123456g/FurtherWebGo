5985688de20f016f1e6b6f9e419b1fc436788e4e
Lennox <lennomugumira12@gmail.com>
2026-06-20 09:04:18 +0300

    added httprouter package

---
+++ greenlight/cmd/api/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declare a string containing the application version number
+// could be generated  at build time
+const version = "1.0.0"
+
+// config struct to hold config settings for the app
+// operatinng envt and port the server will listen on
+// We will read in these
+// configuration settings from command-line flags when the application starts.
+type config struct {
+	port int
+	env  string
+}
+
+//Application Struct to  hold dependencies for HTTP haNDLERS,HELPERS AND middleware
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	//Declare an istance ofthe config struct
+	var cfg config
+
+	//using port 4000  by default  and environment develoment setting sofar
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	//Initialise a new logger to write message to the stdout stream
+	//prefixed with current date and time
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	//APPLICATION DEPENDENCIES
+	//Passing new values to struct , via our new instance
+	//Instance of the applocation struct , containing
+	app := &application{
+		config: cfg,    ///Set the struct's config field to the value of your cfg variable
+		logger: logger, //Set the struct's logger field to the value of your logger variable
+	}
+
+	//Decalre a new servermux and add  /v1/healthcheck route which dispatches requests
+	// to teh healthcheckHandler
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	//SETTING OUR SERVER WITH TIMEOUT SETTINGS
+	//listening to port 4000 in the config struct and the servermux above as the handler
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	//start the HTTP Server
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+
+}
--- greenlight/go.mod
+++ greenlight/go.mod
@@ -2,9 +2,4 @@ module greenlight.lennoxmugumira.net
 
 go 1.25.7
 
-//When there is a valid go.mod file in the root of your project directory, your project is a
-//module.
-//When you’re working inside your project directory and download a dependency with
-//go get, then the exact version of the dependency will be recorded in the go.mod file.
-//Because the exact version is known, this makes it much easier to ensure reproducible
-//builds across different machines and environments.
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
